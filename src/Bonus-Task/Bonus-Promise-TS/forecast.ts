7223915964f3e2bbe8bd4f93a79c4f928a28c19d
DeborahAleaKoch <alea-de@web.de>
2025-01-30 14:39:10 +0100

    Promise Task done

---
+++ src/Bonus-Task/Bonus-Promise-TS/forecast.ts
@@ -0,0 +1,18 @@
+import { WeatherType } from "./app";
+
+const weatherPromise = new Promise((resolve, reject) => {
+	const randomNumber = Math.floor(Math.random() * 10);
+	if (randomNumber <= 6) {
+		return resolve(randomNumber);
+	} else {
+		reject("Weather forecast could not be determined");
+	}
+});
+
+weatherPromise
+	.then((result) => {
+		console.log(result);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
